53ec1ec9763b21ac13899658a8e7868aacd96255
bandicoooot <126168550+bandicoooot@users.noreply.github.com>
2025-07-20 17:42:13 +0200

    Add files via upload
    first commit

---
+++ checkPoint1/Celsius To Farenheit Konverter.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+let celsiusIncome = 25.0
+
+var  fahrenheit = celsiusIncome * 9
+fahrenheit = fahrenheit / 5
+fahrenheit = fahrenheit + 32
+
+print("Celsius:" ,celsiusIncome,"==","fahrenheit:",fahrenheit)
+
+
+
+
+
+
+//avali ro khodam neveshatma
+let Celsius = 25.0
+
+let farenheitt = Celsius * 9 / 5 + 32
+
+print("\(Celsius)˜C == \(farenheitt)˜F")
+

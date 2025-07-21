4e8b2060c54698a7dcb6d48ab071d93a366aea8c
bandicoooot <126168550+bandicoooot@users.noreply.github.com>
2025-07-22 00:53:57 +0200

    Add files via upload
    ba ramze ya zahra mifrestim ke bere

---
+++ checkPoint_2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+let rapAlbums = ["SLIM SHADY" , "eshtebah khoob", "asphalt jungle", "zakhar NAme","SLIM SHADY" ]
+
+rapAlbums.count
+
+dump(rapAlbums)
+Set(rapAlbums)
+
+
+print(Set(rapAlbums))
